910896e3cfff087aa63835e9244cb5aaff29f0e7
aarppe <antti.arppe@iki.fi>
2026-08-01 13:45:51 -0600

    Added sequence of script calls to generate aggregate tags weights based on ALTLab corpora and the CW dictionary.

--- fst/morphology/scripts/compile-hfst-weighted-fsts.sh
+++ fst/morphology/scripts/compile-hfst-weighted-fsts.sh
@@ -19,6 +19,11 @@ echo 'Concatenated LEXC source files into: lexicon.lexc.' ;
 
 echo 'Adding feature weights into LEXC file: lexicon.lexc.' ;
 
+# bin/generate-a-w-b-s-m-wordform-lemma-anl-frequency-list.sh corpora ~/gt/lang-crk/ " -b 2 /Users/arppe/altdev/lang-crk/fst/morphology/fst/analyser-gt-norm.hfstol" > generated/ahenakew_wolfart_bloomfield_supplements_mason.fst+cg.freq-sorted.txt3
+# cat generated/ahenakew_wolfart_bloomfield_supplements_mason.fst+cg.freq-sorted.txt3 | bin/extract-individual-tag-frequencies-from-full-analysis-list.sh > generated/crk_aw_b_s_m.tags_freq.txt
+# bin/extract-tag-frequencies-from-cw-entry-lexicalized-derivations.sh dicts/Wolvengrey_altlab.toolbox /Users/arppe/altdev/lang-crk/fst/morphology/fst/analyser-gt-norm.hfstol '(N|V)' '^(P[VN]/|Der/).+' 0 > generated/crk_cw_pvn_der.tags_freq.txt
+# bin/aggregate-corpus+dictionary-feature-tag-weights.sh generated/crk_aw_b_s_m.tags_freq.txt generated/crk_cw_pvn_der.tags_freq.txt '1+1' > generated/crk_aw_b_s_m_corp+cw_dict.tags_freq.txt
+
 scripts/insert-weights-to-lexc.sh lexicon.lexc scripts/crk_aw_b_s_m_corp+cw_dict.tags_freq.txt log '*2' '=430631' yes > lexicon_weighted.lexc
 
 echo 'Added feature weights into LEXC file: lexicon_weighted.lexc.' ;
